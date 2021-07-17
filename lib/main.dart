9112b60b6975fd1b1539f530f41698a99674116c
sajidshahriar72543 <sazidshahriar39@gmail.com>
2021-07-17 23:44:58 +0600

    New fonts added
    - Poppins-regular
    - OpenSans-regular

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        fontFamily: "Signatra",
+        fontFamily: "Poppins-Regular",
         primarySwatch: Colors.blue,
       ),
       home: MainScreen(),
