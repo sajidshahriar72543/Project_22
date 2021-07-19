e2f984c0d2559c15987d503ca058cf1a98435fff
sajidshahriar72543 <sazidshahriar39@gmail.com>
2021-07-20 00:22:18 +0600

    Initial integration of Google Maps to app
    - Updated flutter
    - Set minimum sdk to 20

--- lib/AllScreens/mainscreen.dart
+++ lib/AllScreens/mainscreen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MainScreen extends StatefulWidget {
   static const String idScreen = "mainScreen";
@@ -9,16 +12,37 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  Completer<GoogleMapController> _controllerGoogleMap = Completer();
+  GoogleMapController newgoogleMapController;
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text(
-          "Main Screen",
+          "Opening Screen",
           textAlign: TextAlign.center,
           style: TextStyle(fontFamily: 'OpenSans-Regular'),
         ),
+      ),
+      body: Stack(
+        children: [
+          GoogleMap(
+            mapType: MapType.normal,
+            myLocationButtonEnabled: true,
+            initialCameraPosition: _kGooglePlex,
+            onMapCreated: (GoogleMapController controller) {
+              _controllerGoogleMap.complete(controller);
+              newgoogleMapController = controller;
+            },
+          ),
+        ],
       ),
     );
   }
---
+++ lib/configMaps.dart
@@ -0,0 +1,1 @@
+String mapkey = "AIzaSyCB082vhSRG_clFvcLm7vUV-Hx-TJ3ySjU";
